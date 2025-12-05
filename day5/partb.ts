b79709227826c652b8f5eca52ad4ae8f4110ee32
JoJo <jonahthwaite@gmail.com>
2025-12-05 16:01:25 +0000

    AoC day 5 - completed

--- day5/partb.ts
+++ day5/partb.ts
@@ -1,22 +1,16 @@
 import rdFile from "../rdFile.ts";
 
-const data = await rdFile("day5/sample-data-b.txt");
+const data = await rdFile("day5/data.txt");
 
-function parseRange(range: string): [number, number] {
-  const minmax = range.split("-");
-  return [+minmax[0], +minmax[1]];
-}
-
-//* Can't have an array with length > 2^32 - 1
 if (typeof data !== "undefined") {
   let ranges = data
     .split("\r\n\r\n")[0]
     .split("\r\n")
-    .map((range) => parseRange(range))
+    .map((range) => range.split("-").map(Number))
     .sort((a, b) => a[0] - b[0]);
   // console.log(ranges);
 
-  for (let i = 0; i < ranges.length; i++) {
+  outerLoop: for (let i = 0; i < ranges.length; i++) {
     const [minI, maxI] = ranges[i];
     for (let j = 0; j < ranges.length; j++) {
       if (i === j) continue;
@@ -24,6 +18,7 @@ if (typeof data !== "undefined") {
 
       // When a a pair of non-disjoint ranges are found, combine them and reset loop
       if (maxI < minJ || minI > maxJ) continue;
+      // console.log(`${i}, ${j}`);
 
       ranges = ranges.filter(
         (range) => range !== ranges[i] && range !== ranges[j]
@@ -32,10 +27,12 @@ if (typeof data !== "undefined") {
       ranges.sort((a, b) => a[0] - b[0]);
       i = 0;
       j = 0;
+      // console.log(ranges);
+      continue outerLoop;
     }
   }
 
   //? Sample should return 14
-  console.log(ranges);
+  // console.log(ranges);
   console.log(ranges.reduce((acc, cur) => acc + (cur[1] - cur[0] + 1), 0));
 }
