0a3da72d7f325f0edf0454bcfb53f9debfd98c14
jonahthwaite <jonahthwaite@gmail.com>
2025-12-06 23:36:33 +0000

    Switch laptop -> PC

--- day6/parta.ts
+++ day6/parta.ts
@@ -2,8 +2,19 @@ import rdFile from "../rdFile.ts";
 
 const data = await rdFile("day6/sample-data-a.txt");
 
+function parseRow(row: string) {
+  return [...row.matchAll(/\d+/g)].map(Number);
+}
+
 if (typeof data !== "undefined") {
   let total = 0;
+  const rows = data.split("\r\n");
+  console.log(rows);
+  const operands: number[] = parseRow(rows[0]);
+  for (const row in rows) {
+  }
+
+  const operators: ("+" | "*")[] = [];
 
   //? Sample should return 4277556
   console.log(total);
