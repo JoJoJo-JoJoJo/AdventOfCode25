5006cded5d1771a389da38141312c46e9102c1f5
JoJoJo-JoJoJo <jonahthwaite@gmail.com>
2025-12-06 16:22:26 +0000

    AoC day 6 - setup

---
+++ day6/parta.ts
@@ -0,0 +1,10 @@
+import rdFile from "../rdFile.ts";
+
+const data = await rdFile("day6/sample-data-a.txt");
+
+if (typeof data !== "undefined") {
+  let total = 0;
+
+  //? Sample should return 4277556
+  console.log(total);
+}
