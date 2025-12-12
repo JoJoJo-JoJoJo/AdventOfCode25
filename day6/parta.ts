852fd6657b2833450674f0aca928aa5feea7a7a2
JoJo <jonahthwaite@gmail.com>
2025-12-12 16:41:49 +0000

    AoC day 6 - part a completed

--- day6/parta.ts
+++ day6/parta.ts
@@ -1,21 +1,49 @@
 import rdFile from "../rdFile.ts";
 
-const data = await rdFile("day6/sample-data-a.txt");
+const data = await rdFile("day6/data.txt");
 
-function parseRow(row: string) {
-  return [...row.matchAll(/\d+/g)].map(Number);
+function parseNumRow(row: string): number[] {
+  const matches = row.matchAll(/\d+/g);
+  if (typeof matches === undefined) return [];
+  return [...matches].map(Number);
+}
+
+function parseStrRow(row: string): string[] {
+  const matches = row.matchAll(/[+*]+/g);
+  if (typeof matches === undefined) return [];
+  return [...matches].map(String);
 }
 
 if (typeof data !== "undefined") {
-  let total = 0;
   const rows = data.split("\r\n");
-  console.log(rows);
-  const operands: number[] = parseRow(rows[0]);
-  for (const row in rows) {
+
+  const operands: number[][] = [];
+  const operators: string[] = [];
+
+  for (let i = 0; i < rows.length; i++) {
+    if (i === rows.length - 1) {
+      operators.push(...parseStrRow(rows[i]));
+    } else {
+      operands.push(parseNumRow(rows[i]));
+    }
   }
 
-  const operators: ("+" | "*")[] = [];
+  const runningTotals: number[] = new Array(operators.length).fill(0);
+
+  for (let i = 0; i < operands.length; i++) {
+    for (let j = 0; j < operators.length; j++) {
+      if (i === 0) {
+        runningTotals[j] = operands[i][j];
+        continue;
+      }
+      if (operators[j] === "+") {
+        runningTotals[j] += operands[i][j];
+      } else if (operators[j] === "*") {
+        runningTotals[j] *= operands[i][j];
+      }
+    }
+  }
 
   //? Sample should return 4277556
-  console.log(total);
+  console.log(runningTotals.reduce((a, b) => a + b, 0));
 }
